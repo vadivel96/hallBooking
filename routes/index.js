cf02f72876902a8e9fcc921ce75723d3749eb042
vadivel96 <gokulvickybarathy@gmail.com>
2023-05-22 11:18:36 +0530

    all

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 
 
 const db=mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: "127.0.0.1",
+  user: "root",
+  password: "Vl632479!!",
   database: 'hallBooking',
   
 
